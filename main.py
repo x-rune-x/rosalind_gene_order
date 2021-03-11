8a24397a2d98f024b31c25bb2ad058ecff3c6d4e
James Bedford <jamesbedford@protonmail.com>
2021-03-11 12:01:41 +1100

    Working solution

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-import math
 from itertools import permutations
 
 
 def get_permutations(n):
-    integers = []
-    for i in range(1, n+1):
-        integers.append(i)
+    # Create a list of numbers from 1 to n.
+    integers = [i for i in range(1, n+1)]
 
-    perm = permutations(integers)
-    for i in perm:
-        print(i)
+    # Permutations function finds the permutations.
+    perms = permutations(integers)
+
+    # Having trouble with permutations object only being usable once. (?) So convert it to list. Can easily grab length
+    # in list form as well.
+    list_perm = list(perms)
+    perm_len = len(list_perm)
+
+    # Rosalind wants the number of permutations listed at the start so create a string beginning with that.
+    out_string = f'{perm_len}'
+    for perm in list_perm:
+        # Rosalind does not want brackets and commas between digits so this nested loop converts every element to a
+        # string before adding it to our output string.
+        str_conv = ''
+        for element in perm:
+            str_conv += str(element) + ' '
+
+        out_string += '\n' + str_conv
+
+    output_text = open("solution.txt", "w")
+    output_text.write(out_string)
+    output_text.close()
+
+    return out_string
 
 
-get_permutations(3)
+solution = get_permutations(7)
+print(solution)
